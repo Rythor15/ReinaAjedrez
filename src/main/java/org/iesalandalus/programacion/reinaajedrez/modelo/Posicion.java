eb2003e601869677df1b2388fd315820572c3dc6
Ruben <Ruben@DESKTOP-1P55R5J>
2022-11-14 12:30:23 +0100

    integracion atributos fila y columna

---
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -0,0 +1,14 @@
+package org.iesalandalus.programacion.reinaajedrez.modelo;
+
+public class Posicion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		
+	}
+
+}
