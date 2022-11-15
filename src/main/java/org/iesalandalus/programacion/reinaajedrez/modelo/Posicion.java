19b693a17e5e0b3a1335c244a3caa8ad45512ce2
Ruben <Ruben@DESKTOP-1P55R5J>
2022-11-15 12:54:55 +0100

    Implementacion getters y setters de fila y columna

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -1,9 +1,34 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
 public class Posicion {
-
+	
 	private int fila;
 	private char columna;
+	
+	//Constructor Copia
+	public Posicion(Posicion posicion) {
+		fila = posicion.getFila();
+		columna = posicion.getColumna();
+	}
+
+	public int getFila() {
+		return fila;
+	}
+	private void setFila(int fila) {
+		if (fila <= 0 || fila > 8 ) {
+			throw new IllegalArgumentException("La fila no tiene un valor adecuado");
+		}
+		this.fila = fila;
+	}
+	public char getColumna() {
+		return columna;
+	}
+	private void setColumna(char columna) {
+		if (columna <= 'a' || columna > 'h') {
+			throw new IllegalArgumentException("La energia no tiene un valor adecuado");
+		}
+		this.columna = columna;
+	}
 	
 	
 }
