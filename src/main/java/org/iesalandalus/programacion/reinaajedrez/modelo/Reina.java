2c89f996d210332139204095915ad33a148f9d60
Ruben <Ruben@DESKTOP-1P55R5J>
2022-11-15 13:58:46 +0100

    Getters and Setters y comprobacion de color

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -6,5 +6,25 @@ public class Reina {
 		private Color color;
 		private Posicion posicion;
 		
+		
+		
+		public Color getColor() {
+			return color;
+		}
+		public Posicion getPosicion() {
+			return posicion;
+		}
+		
+		private void setColor(Color color) {
+			if (color == null) {
+				throw new NullPointerException("ERROR: No es posible copiar un color nula.");
+			}
+			this.color = color;
+		}
+		private void setPosicion(Posicion posicion) {
+			this.posicion = posicion;
+		}
+		
+		
 	}
 
