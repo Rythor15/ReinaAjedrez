4e6bd88cc52f4a7974f19fb18192354863047d44
Ruben <Ruben@DESKTOP-1P55R5J.home>
2022-11-16 18:00:28 +0100

    Implementacion atributo de clase reina

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -1,8 +1,11 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import org.iesalandalus.programacion.reinaajedrez.modelo.Reina;
+
 public class MainApp {
-	
+
 	public static void main(String[] args) {
-		
+
+		Reina reina;
 	}
 }
