82b50ff1f86883c368e49cdb36737322f5811ef7
Ruben <Ruben@DESKTOP-1P55R5J>
2022-11-16 13:51:41 +0100

    Crecion mostrarMenu

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
@@ -3,7 +3,23 @@ package org.iesalandalus.programacion.reinaajedrez;
 public class Consola {
 
 	private Consola() {
-
+ 
 	}
 
+	
+	public void mostrarMenu() {
+		System.out.println("-------------------------------------");
+		System.out.println("");
+		System.out.println("Menu Incial:");
+		System.out.println("");
+		System.out.println("-------------------------------------");
+		System.out.println("");
+		System.out.println("1.- Crear reina por defecto");
+		System.out.println("2.- Crear reina eligiendo el color");
+		System.out.println("3.- Mover");
+		System.out.println("4.- Salir");
+		System.out.println("");
+		System.out.println("-------------------------------------");
+		
+	}
 }
