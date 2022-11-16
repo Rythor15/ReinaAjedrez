8dc49bb0843ab5624534f4ad1c154fb5222bcdbb
Ruben <Ruben@DESKTOP-1P55R5J>
2022-11-16 14:20:42 +0100

    Metodo elegirOpcionMenu

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
@@ -1,12 +1,13 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class Consola {
 
 	private Consola() {
- 
+
 	}
 
-	
 	public void mostrarMenu() {
 		System.out.println("-------------------------------------");
 		System.out.println("");
@@ -20,6 +21,19 @@ public class Consola {
 		System.out.println("4.- Salir");
 		System.out.println("");
 		System.out.println("-------------------------------------");
-		
+
 	}
+
+	public int elegirOpcionMenu() {
+		int opcion;
+		do {
+			System.out.println("-------------------------------------");
+			System.out.println("Seleccione una opcion");
+			System.out.println("-------------------------------------");
+			opcion = Entrada.entero();
+		} while (opcion < 1 && opcion > 4);
+		System.out.print("Su opción escogida fue: ");
+		return Entrada.entero();
+	}
+
 }
