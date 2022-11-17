c4c6e385bb0749e8c94deb1fc8e705d5cb684a32
Ruben <Ruben@DESKTOP-1P55R5J>
2022-11-17 08:45:35 +0100

    Inplementacion metodo mover en MainApp

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import javax.naming.OperationNotSupportedException;
+
 import org.iesalandalus.programacion.reinaajedrez.modelo.Reina;
 
 public class MainApp {
@@ -15,23 +17,32 @@ public class MainApp {
 			crearReinaColor();
 			break;// Crear reina eligiendo el color
 		case 3:
+			mover();
 			break;// Mover
 		case 4:
 			Consola.despedirse();
 			break;// Salir
 		}
 	}
-	
+
 	public static void crearReinaPorDefecto() {
 		reina = new Reina();
 	}
+
 	public static void crearReinaColor() {
 		reina = new Reina(Consola.elegirColor());
 	}
-	
-	
-	
-	
+
+	public static void mover() {
+		Consola.mostrarMenuDirecciones();
+		Consola.elegirDireccion();
+		try {
+			reina.mover(null, 0);
+		} catch (OperationNotSupportedException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}
+	}
 
 	public static void main(String[] args) {
 	}
